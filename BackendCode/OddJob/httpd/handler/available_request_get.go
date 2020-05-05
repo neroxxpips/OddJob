890f706e190f1eabd96c6a44e75cd9def560ed26
ishapirov <47495391+ishapirov@users.noreply.github.com>
2020-05-04 20:04:59 -0400

    Add files via upload

--- BackendCode/OddJob/httpd/handler/available_request_get.go
+++ BackendCode/OddJob/httpd/handler/available_request_get.go
@@ -13,9 +13,9 @@ func AvailableRequestGet(db *sql.DB) gin.HandlerFunc {
 
 		requestid := c.DefaultQuery("requestID", "0")
 		var request request.RequestNoAccepted
-		query := "SELECT request_id,title,post,price,number,street,state,city,zip,task,poster_id FROM requests WHERE request_id=?"
+		query := "SELECT request_id,title,post,price,number,street,state,city,zip,task,reqimg,poster_id FROM requests WHERE request_id=?"
 
-		err := db.QueryRow(query, requestid).Scan(&request.RequestID, &request.Title, &request.Post, &request.Price, &request.Number, &request.Street, &request.State, &request.City, &request.Zipcode, &request.Task, &request.UserID)
+		err := db.QueryRow(query, requestid).Scan(&request.RequestID, &request.Title, &request.Post, &request.Price, &request.Number, &request.Street, &request.State, &request.City, &request.Zipcode, &request.Task, &request.Image, &request.UserID)
 		if err != nil {
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
